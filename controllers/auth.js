e09a62aac1499a81ae3875290ced208a917e4c3a
Débora Camilly <debcamilly00@gmail.com>
2024-03-19 10:39:05 -0300

    finaliza registro de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,32 +1,30 @@
-const express = require ('express');
-const bcrypt = require ('bcryptjs');
-const jwt = require ('jsonwebtoken');
-const Usuario = require('../models/Usuario');
 //user registration
+document.addEventListener('DOMContentLoaded', function() {
   const cadastrar = document.getElementById('button-cadastrar');
   cadastrar.addEventListener('click', async (event) => {
     event.preventDefault();
     await salvar();
   });
-  
-  async function salvar() {
-    const obj = {
-      nome: document.getElementById('name').value,
-      email: document.getElementById('email').value,
-      senha: document.getElementById('password').value
+});
+
+async function salvar() {
+  const obj = {
+    nome: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    senha: document.getElementById('password').value
   };
   console.log(obj);
     
-    await fetch("//localhost:3000/usuarios", {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(obj)
-    });
-    document.getElementById('name').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('password').value = '';
-  }
+  await fetch("//localhost:3000/usuarios", {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(obj)
+  });
+  document.getElementById('name').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('password').value = '';
+}
