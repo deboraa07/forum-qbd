ad3b4d34d2816315b6f36795f1b9ea779171e0a7
Débora Camilly <debcamilly00@gmail.com>
2024-04-02 19:52:34 -0300

    inicia frontend do crud de posts

---
+++ posts/posts.js
@@ -0,0 +1,159 @@
+window.onload = () => {
+    const postsDiv = document.getElementById('posts');
+    const btnListar = document.getElementById('btnListar');
+    const btnPesquisar = document.getElementById('btnPesquisar');
+    let listaVisivel = false;
+
+    btnListar.addEventListener('click', () => {
+      if (listaVisivel) {
+        postsDiv.innerHTML = '';
+        listaVisivel = false;
+      } else {
+     //lista os posts
+     fetch('//localhost:3000/posts')
+     .then(response => response.json())
+     .then(posts => {
+       postsDiv.innerHTML = '';
+
+       posts.forEach(post => {
+         const postDiv = document.createElement('div');
+         postDiv.classList.add('post-container');
+
+         const autorP = document.createElement('p');
+         autorP.textContent = `Autor: ${post.autor}`;
+         postDiv.appendChild(autorP);
+
+         const tituloP = document.createElement('p');
+         tituloP.textContent = `Título: ${post.titulo}`;
+         postDiv.appendChild(tituloP);
+
+         const conteudoP = document.createElement('p');
+         conteudoP.textContent = `Conteúdo: ${post.conteudo}`;
+         postDiv.appendChild(conteudoP);
+         
+         postsDiv.appendChild(postDiv);
+       
+
+         //deletar posts
+
+        const deleteButton = document.createElement('button');
+            deleteButton.classList.add('delete-button');
+            deleteButton.textContent = 'Deletar';
+            deleteButton.addEventListener('click', () => {
+              fetch(`//localhost:3000/posts/${post._id}`, { method: 'DELETE' })
+                .then(response => response.text())
+                .then(message => {
+                  console.log(message); 
+                  btnListar.click();
+                })
+                .catch(error => {
+                  console.error(error);
+                });
+            });
+            postDiv.appendChild(deleteButton);
+
+            //atualiza post
+
+            const updateButton = document.createElement('button');
+            updateButton.classList.add('update-button');
+            updateButton.textContent = 'Atualizar';
+            updateButton.addEventListener('click', () => {
+              const modalContainer = document.getElementById('modalContainer');
+              modalContainer.style.display = 'block';
+
+              document.getElementById('inputAutor').value = post.autor;
+              document.getElementById('inputTitulo').value = post.titulo;
+              document.getElementById('inputConteudo').value = post.conteudo;
+              
+
+              const salvarBtn = document.getElementById('salvarBtn');
+              salvarBtn.addEventListener('click', () => {
+                const novoAutor = document.getElementById('inputAutor').value;
+                const novoTitulo = document.getElementById('inputTitulo').value;
+                const novoConteudo = document.getElementById('inputConteudo').value;
+               
+
+                const dadosAtualizados = {
+                  autor: novoAutor !== '' ? novoAutor : post.autor,
+                  titulo: novoTitulo !== '' ? novoTitulo : post.titulo,
+                  conteudo: novoConteudo !== '' ? novoConteudo : post.conteudo,
+                };
+
+                fetch(`//localhost:3000/posts/${post._id}`, {
+                  method: 'PUT',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify(dadosAtualizados)
+                })
+                  .then(response => response.text())
+                  .then(message => {
+                    console.log(message); // Exibe a mensagem de sucesso ou erro no console
+                    btnListar.click();  // Atualiza a lista de eventos
+                  })
+                  .catch(error => {
+                    console.error(error);
+                  });
+
+                modalContainer.style.display = 'none';
+              });
+
+              const cancelarBtn = document.getElementById('cancelarBtn');
+              cancelarBtn.addEventListener('click', () => {
+                modalContainer.style.display = 'none';
+              });
+            });
+            postDiv.appendChild(updateButton);
+          });
+
+          listaVisivel = true;
+        })
+        .catch(error => {
+          postsDiv.textContent = 'Erro ao carregar posts';
+          console.error(error);
+        });
+      }
+    });
+
+    //pesquisa de posts
+
+    btnPesquisar.addEventListener('click', () => {
+      const textSearch = prompt('Digite o termo de busca:');
+      if (textSearch) {
+        postsDiv.innerHTML = 'Pesquisando...';
+  
+        fetch(`//localhost:3000/posts/${textSearch}`)
+          .then(response => response.json())
+          .then(posts=> {
+            postsDiv.innerHTML = '';
+  
+            if (posts.length === 0) {
+              const mensagemP = document.createElement('p');
+              mensagemP.textContent = 'Nenhum post encontrado.';
+              postsDiv.appendChild(mensagemP);
+            } else {
+              posts.forEach(post => {
+                const postDiv = document.createElement('div');
+                postDiv.classList.add('post-container');
+  
+  
+                const autorP = document.createElement('p');
+                autorP.textContent = `Autor: ${post.nome}`;
+                postDiv.appendChild(autorP);
+  
+                const tituloP = document.createElement('p');
+                tituloP.textContent = `Título: ${post.titulo}`;
+                postDiv.appendChild(tituloP);
+
+                const conteudoP = document.createElement('p');
+                conteudoP.textContent = `Conteúdo: ${post.conteudo}`;
+                postDiv.appendChild(conteudoP);
+
+                postsDiv.appendChild(postDiv);
+              });
+            }
+            listaVisivel = true;
+          });
+      }
+    });
+  };
