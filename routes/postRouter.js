78d25973b95340f53e98e4e860381d45309e01d4
Débora Camilly <debcamilly00@gmail.com>
2024-04-17 10:10:37 -0300

    add funcao de validacao de token

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,15 +1,31 @@
-var express = require('express');
-var postRouter = express.Router();
-
+const express = require('express');
+const postRouter = express.Router();
 const postController = require('../controllers/postController.js');
+const authConfig = require("../config/auth");
+const jwt = require ('jsonwebtoken');
 
 
-//USUARIO//
+const verifyToken = (req,res,next) => {
+  const token = req.headers['authorization'];
+
+  if(!token){
+    return res.status(403).json({message:'Token nao fornecido'});
+  }
+  jwt.verify(token, authConfig.secret, (err,decoded) => {
+    if(err) {
+      return res.status(401).json({message:'Falha na autenticaçao'});
+  }req.user = decoded;
+  next();
+});
+};
+
+
+
 postRouter.get('/', postController.listarPosts);
 postRouter.get('/:conteudo',postController.buscarPorConteudo);
 postRouter.post('/', postController.salvarPost);
-postRouter.delete('/:id',postController.deletarPost);
+postRouter.delete('/:id', postController.deletarPost);
 postRouter.put('/:id', postController.atualizarPost);
 
 
-module.exports = postRouter;
+module.exports = postRouter,verifyToken;
